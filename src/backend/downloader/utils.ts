3353ed12735f54631306815241e464a70572570d
Tiago <tiago.simionato5@gmail.com>
2026-05-29 14:09:17 -0300

    fix: join multiple artists

--- src/backend/downloader/utils.ts
+++ src/backend/downloader/utils.ts
@@ -12,7 +12,7 @@ export const getArtistLabel = (
 
   return credits
     .map(
-      credit => `${credit.name ?? credit.artist?.name ?? 'Unknown'}${credit.joinphrase ?? ''}`,
+      credit => `${credit.name ?? credit.artist?.name ?? 'Unknown'}`,
     )
     .join('; ');
 };
