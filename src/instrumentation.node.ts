9731860bac223cf65bd64529543949f2bde8c4d6
Tiago <tiago.simionato5@gmail.com>
2026-06-25 14:38:26 -0300

    fix: remove uncessary instrumentation file

--- src/instrumentation.node.ts
+++
@@ -1,5 +0,0 @@
-import { scanDownloadedReleasesFromDisk } from 'backend/downloads';
-
-export const register = async () => {
-  await scanDownloadedReleasesFromDisk();
-};
--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,6 @@
 export const register = async () => {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    const nodeInstrumentation = await import('./instrumentation.node');
-    await nodeInstrumentation.register();
+    const { scanDownloadedReleasesFromDisk } = await import('backend/downloads');
+    await scanDownloadedReleasesFromDisk();
   }
 };
