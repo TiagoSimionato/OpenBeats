20dea7cb07bd7d1e5e1353d060dc1d57e52c72b8
Tiago <tiago.simionato5@gmail.com>
2026-07-08 16:32:10 -0300

    fix: header gap

--- src/frontend/ui/Header/Header.tsx
+++ src/frontend/ui/Header/Header.tsx
@@ -3,7 +3,7 @@ import { SignOutButton } from '../SignOutButton';
 import { Nav } from './Nav';
 
 export const Header = () => (
-  <header className="flex items-center gap-8">
+  <header className="flex items-center gap-8 pb-4">
     <h1 className="text-2xl font-semibold">OpenBeats</h1>
     <Nav />
     <ScanLibraryButton />
