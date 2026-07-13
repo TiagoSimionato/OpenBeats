d83b99fe2b3442059037f421408a435b220fccbf
Tiago <tiago.simionato5@gmail.com>
2026-07-13 11:39:35 -0300

    fix: only scan db if file dont exist

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,10 +1,17 @@
 export const register = async () => {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('configs/loggger');
+    const { existsSync } = await import('node:fs');
+    const { CONFIGS } = await import('configs/constants');
     const { dbService } = await import('backend/services/db.service');
-    dbService.initDatabase();
 
-    const { releasesRepository } = await import('backend/repositories/releases.repository');
-    await releasesRepository.scanReleasesFromDisk();
+    if (!existsSync(CONFIGS.DB_PATH)) {
+      await dbService.initDatabase();
+      const { releasesRepository } = await import('backend/repositories/releases.repository');
+      await releasesRepository.scanReleasesFromDisk();
+    }
+    else {
+      await dbService.initDatabase();
+    }
   }
 };
