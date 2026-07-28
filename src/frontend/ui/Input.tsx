967ac00a1888cc00855113738922b1634a1c1c99
Tiago <tiago.simionato5@gmail.com>
2026-07-28 12:39:47 -0300

    feat: input with ref

--- src/frontend/ui/Input.tsx
+++ src/frontend/ui/Input.tsx
@@ -1,5 +1,5 @@
-import type { InputHTMLAttributes } from 'react';
+import type { ComponentPropsWithRef } from 'react';
 
-export const Input = ({ className, ...rest }: InputHTMLAttributes<HTMLInputElement>) => (
+export const Input = ({ className, ...rest }: ComponentPropsWithRef<'input'>) => (
   <input className={`rounded-md border-2 border-zinc-200 px-3 py-2 ${className}`} {...rest} />
 );
