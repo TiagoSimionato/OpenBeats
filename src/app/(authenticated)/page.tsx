8be68d76c2f9692a18b63a753f60be7b4338b691
Tiago <tiago.simionato5@gmail.com>
2026-08-11 11:49:55 -0300

    refactor: library page defaults on app folder

--- src/app/(authenticated)/page.tsx
+++ src/app/(authenticated)/page.tsx
@@ -1,1 +1,21 @@
-export { LibraryPage as default } from 'frontend/pages/Library/Library';
+import { releasesRepository } from 'backend/repositories/releases.repository';
+import { ReleasesFilters } from 'backend/requests/releases';
+import { buildDTO, buildPagination } from 'backend/utils';
+import { LibraryPage } from 'frontend/pages/Library/Library';
+
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
+
+export const Page = async ({ searchParams }: { searchParams: SearchParams }) => {
+  const params = await searchParams;
+
+  const pagination = await buildPagination(params);
+  const filters = await buildDTO(ReleasesFilters, params, false);
+
+  const libraryReleases = await releasesRepository.listLibraryReleasesPaged(pagination, filters);
+
+  return (
+    <LibraryPage defaultPages={libraryReleases.pages} defaultReleases={libraryReleases.data} />
+  );
+};
+
+export default Page;
--- src/frontend/pages/Library/Library.tsx
+++ src/frontend/pages/Library/Library.tsx
@@ -1,19 +1,56 @@
-import { releasesRepository } from 'backend/repositories/releases.repository';
-import { ReleasesFilters } from 'backend/requests/releases';
-import { buildDTO, buildPagination } from 'backend/utils';
-import { LibraryListing } from './components/LibraryList';
+'use client';
 
-type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
+import type { LibraryReleaseData } from 'common/types/requests/library';
+import { useGetLibrary } from 'frontend/services/api/queries/library';
+import { PaginationControls } from 'frontend/ui/PaginationControls';
+import { useEffect, useRef } from 'react';
+import { LibraryFilters } from './components/LibraryFilters';
+import { NoReleasesFound } from './components/NoReleasesFound';
+import { ReleaseCard } from './components/ReleaseCard';
+import { useGridWidthStore } from './store/useGridWidthStore';
 
-export const LibraryPage = async ({ searchParams }: { searchParams: SearchParams }) => {
-  const params = await searchParams;
+type LibraryListingProps = {
+  defaultPages: number;
+  defaultReleases: LibraryReleaseData[];
+};
 
-  const pagination = await buildPagination(params);
-  const filters = await buildDTO(ReleasesFilters, params, false);
+export const LibraryPage = ({ defaultPages, defaultReleases }: LibraryListingProps) => {
+  const { data: libraryData } = useGetLibrary();
+  const { setWidth, width } = useGridWidthStore();
+  const gridRef = useRef<HTMLDivElement>(null);
 
-  const libraryReleases = await releasesRepository.listLibraryReleasesPaged(pagination, filters);
+  const releases = libraryData?.data ?? defaultReleases;
+
+  useEffect(() => {
+    if (gridRef.current?.clientWidth)
+      setWidth(gridRef.current.clientWidth);
+  }, [setWidth]);
+
+  const colSize = 200;
+  const cardPadding = 16;
+  const isSingleLine = width && releases.length * (colSize + cardPadding) < width;
 
   return (
-    <LibraryListing defaultPages={libraryReleases.pages} defaultReleases={libraryReleases.data} />
+    <main className="flex grow flex-col gap-4">
+      <LibraryFilters />
+      {releases.length === 0 && <NoReleasesFound />}
+      {releases.length > 0 && (
+        <div
+          className="flex flex-wrap sm:grid"
+          ref={gridRef}
+          style={{
+            gridTemplateColumns: isSingleLine
+              ? `repeat(auto-fit, ${colSize + cardPadding}px)`
+              : `repeat(auto-fit, minmax(${colSize}px, 1fr))`,
+          }}
+        >
+          {width && releases.map(release => <ReleaseCard key={release.id} release={release} />)}
+        </div>
+      )}
+      <PaginationControls
+        className={releases.length === 0 ? 'hidden' : ''}
+        pages={libraryData?.pages ?? defaultPages}
+      />
+    </main>
   );
 };
