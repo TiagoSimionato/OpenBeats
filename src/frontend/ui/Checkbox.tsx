3427bdbc9252fa90b1ad56a9d16747aff8646cdd
Tiago <tiago.simionato5@gmail.com>
2026-08-19 13:00:35 -0300

    fix: checkbox min size

--- src/frontend/ui/Checkbox.tsx
+++ src/frontend/ui/Checkbox.tsx
@@ -4,7 +4,7 @@ type CheckboxProps = ComponentProps<'input'>;
 
 export const Checkbox = ({ className, ...props }: CheckboxProps) => (
   <input
-    className={`h-5.5 w-5.5 cursor-pointer rounded-sm border-2 not-checked:appearance-none ${className}`}
+    className={`h-5.5 min-h-5.5 w-5.5 min-w-5.5 cursor-pointer rounded-sm border-2 not-checked:appearance-none ${className}`}
     type="checkbox"
     {...props}
   />
